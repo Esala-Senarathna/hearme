2f60a01fc4ba4bf81c47ca6a6f3faa7c954ac419
ple98 <lelkadaparami@gmail.com>
2023-05-26 00:02:31 +0530

    feature slider

---
+++ index.js
@@ -0,0 +1,17 @@
+let images = ["assets/phone2.png", "assets/phone1.png"]; // Add your images paths here
+let texts = ["Interactive User Interface", "Child Friendly Games"]; // Add your texts here
+
+let currentImageIndex = 0;
+let currentTextIndex = 0;
+
+// Change the image inside the phone frame every 3 seconds
+setInterval(function() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    document.getElementById('slideshow-image').src = images[currentImageIndex];
+}, 3000);
+
+// Change the text on the right side of the screen every 5 seconds
+setInterval(function() {
+    currentTextIndex = (currentTextIndex + 1) % texts.length;
+    document.getElementById('slideshow-text').textContent = texts[currentTextIndex];
+}, 3000);
